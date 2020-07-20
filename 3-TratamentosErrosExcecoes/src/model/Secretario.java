2ed868bb92e0a3d27c741b68a3fe6f0d96ae2929
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-07-20 00:14:49 -0300

    Utilizando TryCatch para tratamento de erros e exceções

---
+++ 3-TratamentosErrosExcecoes/src/model/Secretario.java
@@ -0,0 +1,80 @@
+package model;
+
+import interfaces.PermitirAcesso;
+
+public class Secretario extends Pessoa implements PermitirAcesso {
+
+	private String registro;
+	private String nivelCargo;
+	private String experiencia;
+	
+	private String login;
+	private String senha;
+	
+	/*Construtor para receber login e senha do secretário*/
+	public Secretario(String login, String senha) {
+		
+	}
+	
+	public Secretario() {}
+	
+	public String getRegistro() {
+		return registro;
+	}
+	
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+	
+	public String getNivelCargo() {
+		return nivelCargo;
+	}
+	
+	public void setNivelCargo(String nivelCargo) {
+		this.nivelCargo = nivelCargo;
+	}
+	public String getExperiencia() {
+		return experiencia;
+	}
+	
+	public void setExperiencia(String experiencia) {
+		this.experiencia = experiencia;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Secretario [registro=" + registro + ", nivelCargo=" + nivelCargo + ", experiencia=" + experiencia
+				+ ", nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai + "]";
+	}
+	
+	/*Médoto abstrado da classe pai(Pessoa)*/
+	@Override
+	public double salario() {
+		return 2800 * 0.9;
+	}
+	
+	/*Método do contrato de autenticação da interface*/
+	@Override
+	public boolean autenticar(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+		return login.equals("admin") && senha.equals("admin");
+	}
+}
